e4374a4ffe398318612ce920a4bea8fc4a2c3fa9
JAUSLIN Raphaël <raphael.jauslin@unine.ch>
2021-02-03 14:55:07 +0100

    distmat

--- R/distMat.R
+++ R/distMat.R
@@ -9,11 +9,17 @@
 #' @examples
 #' N <- 36
 #' X <- as.matrix(expand.grid(1:sqrt(N),1:sqrt(N)))
-#' distMat(X,tore = TRUE,toreBound = 6)
+#' N <- nrow(X)
+#' tb = runif(2)/100
+#' #f <- function(k,Xcoord,tore,toreBound){distUnitk(as.matrix(Xcoord),k =k,tore, toreBound)}
+#' #system.time(do.call(cbind,lapply(X = 1:N,FUN = f,Xcoord = X,tore = TRUE,toreBound = sqrt(N))))
+#' system.time(D <- distMat(X,ref = 1,tb,tore = TRUE,toreBound = sqrt(N)))
 #'
-distMat <- function(X,tore = FALSE,toreBound = 0){
+distMat <- function(X,ref,tb,tore = FALSE,toreBound = 0){
   N <- nrow(X)
   d <- array(rep(0,N*N),c(N,N))
+  tmp = X[ref,]
+  X[ref,] = X[ref,] + tb
   if(tore != 0){
     for(i in 1:N){
       d[i,] <- distUnitk(as.matrix(X),k =i,tore = TRUE, toreBound = toreBound)
@@ -23,6 +29,6 @@ distMat <- function(X,tore = FALSE,toreBound = 0){
       d[i,] <- distUnitk(as.matrix(X),k =i,tore = FALSE, toreBound = 0)
     }
   }
-
+  X[ref,] = tmp;
   return(d)
 }
